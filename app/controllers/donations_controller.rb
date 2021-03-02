b7fe74542c8c639ff93a79721ee8fec04ff851bd
Mhd Adel Al Masri <mhdadelalmasri@gmail.com>
2021-03-02 17:08:26 +0100

    added the form

--- app/controllers/donations_controller.rb
+++ app/controllers/donations_controller.rb
@@ -4,8 +4,24 @@ class DonationsController < ApplicationController
   end
 
   def new
+    @club = Club.find(params[:club_id])
+    @donation = Donation.new
   end
 
   def create
+    @club = Club.find(params[:club_id])
+    @donation = Donation.new(donation_params)
+    @donation.club = @club
+    if @donation.save
+      redirect_to club_path(@club)
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def donation_params
+    params.require(:donation).permit(:amount)
   end
 end
