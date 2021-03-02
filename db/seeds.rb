7f72d31e2f94238049c8e1a7ebd24f514392e08d
Andreas Broska <a.broska@gmx.net>
2021-03-02 14:06:30 +0100

    seed creation

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+require 'faker'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +6,57 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Destorying the database..."
+User.destroy_all
+Club.destroy_all
+Donation.destroy_all
+Species.destroy_all
+ClubSpecy.destroy_all
+puts "Database destroyed!"
+puts "Creating the Users..."
+
+users = []
+users << User.create(email: "seb@lewagon.org", encrypted_password: "123456", address: "Tempelhofer Strasse 5", goal: "125.75" )
+users << User.create(email: "timo@home.org", encrypted_password: "123456", address: "Samsungweg 78", goal: "326.89" )
+users << User.create(email: "winnie@puh.org", encrypted_password: "123456", address: "Honigweg 7", goal: "425.56" )
+puts "Users created! Test: #{users.first.email}"
+
+puts "Creating some Clubs"
+
+clubs = []
+
+clubs << Club.create(user_id: users[0], name: "A Better Place", description: "Wir helfen Hunden, die keiner mehr wollte. Hunde, die den Besitzern zu alt und zu krank wurden. Hunde, die misshandelt wurden. Mit deiner Hilfe möchten wir diesen armen Geschöpfen einen würdigen Lebensabend mit viel Liebe ermöglichen.", address: "Gutstrasse 31", category: ["rescue", "food"], instagram_link: "www.instagram.com", facebook_link: "www.facebook.com", website_link: "www.google.com")
+clubs << Club.create(user_id: users[1], name: "HelpDotOrg", description: "Wir wollen mit unserem Projekt DAGI DOG in Dagestans Hauptstadt Machatschkala aktiv helfen, in Form eines umfassenden Kastrationsprogramms und der Pflege verletzter Tiere in einem dafür bereits angemieteten Haus.", address: "Gutstrasse 31", category: ["rescue", "food"], instagram_link: "www.instagram.com", facebook_link: "www.facebook.com", website_link: "www.google.com")
+clubs << Club.create(user_id: users[2], name: "Mamma's 4 Kitties", description: "Warum brauchen die Tiere in Dagestan unsere Hilfe?
+Die russische Republik Dagestan liegt idyllisch zwischen zwei Meeren, die Hauptstadt ist Machatschkala. Von dieser in Russland doch noch relativ neuen Sache namens „Tierschutz” haben hier leider noch nicht so viele Leute etwas gehört. Es gibt kein Tierheim, nicht einmal ein Staatliches. Überall laufen Straßentiere herum und vermehren sich unkontrolliert. Man schätzt ihren Bestand auf mindestens 200.000 Hunde und Katzen. Es gibt keine Kastrationsprogramme, nur Vergiftungsaktionen. Besonders an den Bahnhöfen wimmelt es von Tieren, da hier Reisende vorbei kommen und ab und zu ihr Essen teilen. So dicht an den Gleisen kommt es ständig zu Unfällen. Hunde und Katzen mit fehlenden Gliedmaßen anzutreffen, ist hier Usus. Natürlich gibt es auch einzelne Tierschützer, die sich seit Jahren vor Ort engagieren und ihr letztes Hemd den Tieren geben. Aber ohne Unterstützung der Bevölkerung, des Staates oder einer großen Organisation kommen sie nicht weit. Wenn die Tiere kaum jemanden in der Bevölkerung interessieren, spendet auch niemand. „Tierschutz” ist hier noch ein relativ neuer Trend, von dem noch keiner so recht weiß, ob er ihn mitmachen will.", address: "Gutstrasse 31", category: ["rescue", "food"], instagram_link: "www.instagram.com", facebook_link: "www.facebook.com", website_link: "www.google.com")
+puts "Clubs created Test: #{clubs.first.name}"
+
+puts "creating Donations"
+
+users.each do |user|
+  3.times do
+    Donation.create(amount: rand(0..45).to_f, user_id: user.id, club_id: clubs[rand(0...2)].id )
+  end
+end
+
+puts "Donations created"
+
+puts "create Species"
+
+animalspecies = []
+
+9.times do
+  animalspecies << Species.create(name: Faker::Creature::Animal.name )
+end
+puts "Species done"
+
+puts "create Club Species"
+
+clubs.each do |club|
+  2.times do
+    ClubSpecy.create(club_id: club.id, species_id: animalspecies[rand(0..animalspecies.size - 1)].id)
+  end
+end
+
+puts "Club Species created"
+puts "Seeds complete!"
