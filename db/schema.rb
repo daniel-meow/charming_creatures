d2be9044d213403c390ac6f275251836c2a1e804
Leonard Diemer <leonard.ow.diemer@gmail.com>
2021-03-03 16:05:49 +0100

    plus comments

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2021_03_03_105608) do
-
+ActiveRecord::Schema.define(version: 2021_03_03_114100) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
