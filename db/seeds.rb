0aff0daf4b3e0d63d998ecdd00afe769b7abadde
Mhd Adel Al Masri <mhdadelalmasri@gmail.com>
2021-03-04 16:27:28 +0100

    new-seed-is-done

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,50 +18,52 @@ puts "Creating the Users..."
 
 users = []
 
-20.times do
-  users << User.create(email: Faker::Internet.email ,password: "123456", address: Faker::Address.street_address)
+30.times do
+  users << User.create(email: Faker::Internet.email, password: "123456", first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, address: Faker::Address.street_address)
 end 
 puts "Users created! Test: #{users.first.email}"
 
-puts "Creating some Clubs"
-clubs = []
 
-clubs << Club.create(user_id: users[0].id, name: "A Better Place", description: "Wir helfen Hunden, die keiner mehr wollte. Hunde, die den Besitzern zu alt und zu krank wurden. Hunde, die misshandelt wurden. Mit deiner Hilfe möchten wir diesen armen Geschöpfen einen würdigen Lebensabend mit viel Liebe ermöglichen.", address: "Gutstrasse 31", category: ["rescue", "food"], instagram_link: "www.instagram.com", facebook_link: "www.facebook.com", website_link: "www.google.com")
-clubs << Club.create(user_id: users[1].id, name: "HelpDotOrg", description: "Wir wollen mit unserem Projekt DAGI DOG in Dagestans Hauptstadt Machatschkala aktiv helfen, in Form eines umfassenden Kastrationsprogramms und der Pflege verletzter Tiere in einem dafür bereits angemieteten Haus.", address: "Gutstrasse 31", category: ["rescue", "food"], instagram_link: "www.instagram.com", facebook_link: "www.facebook.com", website_link: "www.google.com")
-clubs << Club.create(user_id: users[2].id, name: "Mamma's 4 Kitties", description: "Warum brauchen die Tiere in Dagestan unsere Hilfe?
-Die russische Republik Dagestan liegt idyllisch zwischen zwei Meeren, die Hauptstadt ist Machatschkala. Von dieser in Russland doch noch relativ neuen Sache namens „Tierschutz” haben hier leider noch nicht so viele Leute etwas gehört. Es gibt kein Tierheim, nicht einmal ein Staatliches. Überall laufen Straßentiere herum und vermehren sich unkontrolliert. Man schätzt ihren Bestand auf mindestens 200.000 Hunde und Katzen. Es gibt keine Kastrationsprogramme, nur Vergiftungsaktionen. Besonders an den Bahnhöfen wimmelt es von Tieren, da hier Reisende vorbei kommen und ab und zu ihr Essen teilen. So dicht an den Gleisen kommt es ständig zu Unfällen. Hunde und Katzen mit fehlenden Gliedmaßen anzutreffen, ist hier Usus. Natürlich gibt es auch einzelne Tierschützer, die sich seit Jahren vor Ort engagieren und ihr letztes Hemd den Tieren geben. Aber ohne Unterstützung der Bevölkerung, des Staates oder einer großen Organisation kommen sie nicht weit. Wenn die Tiere kaum jemanden in der Bevölkerung interessieren, spendet auch niemand. „Tierschutz” ist hier noch ein relativ neuer Trend, von dem noch keiner so recht weiß, ob er ihn mitmachen will.", address: "Gutstrasse 31", category: ["rescue", "food"], instagram_link: "www.instagram.com", facebook_link: "www.facebook.com", website_link: "www.google.com")
+puts "Creating some Clubs"
+
+category = ['medical', 'rescue', 'food', 'transportaion', 'educations']
+clubs = []
+users.each do |user|
+  clubs << Club.create(user_id: user.id, name: Faker::FunnyName.name, description: "Wir helfen Hunden, die keiner mehr wollte. Hunde, die den Besitzern zu alt und zu krank wurden. Hunde, die misshandelt wurden. Mit deiner Hilfe möchten wir diesen armen Geschöpfen einen würdigen Lebensabend mit viel Liebe ermöglichen.", address: Faker::Address.street_address, category: category.sample, instagram_link: "www.instagram.com", facebook_link: "www.facebook.com", website_link: "www.google.com")
+end
 puts "Clubs created Test: #{clubs.first.name}"
+
 
 puts "creating Donations"
 
 users.each do |user|
-  3.times do
-    Donation.create(amount: rand(0..45).to_f, user_id: user.id, club_id: clubs[rand(0...2)].id )
-  end
+    Donation.create(amount: rand(0..45).to_f, user_id: user.id, club_id: clubs[rand(1...30)].id )
 end
-
 puts "Donations created"
+
 
 puts "create Species"
 
-animalspecies = []
-
-9.times do
-  animalspecies << Species.create(name: Faker::Creature::Animal.name )
+animal_species = ['dogs', 'cats', 'dolphins', 'dinasaurs', 'elephants', 'birds', 'wolfs', 'panda', 'raccon']
+species = []
+animal_species.each do |specie|
+  species << Species.create(name: specie)
 end
 puts "Species done"
+
 
 puts "create Club Species"
 
 clubs.each do |club|
   2.times do
-    ClubSpecy.create(club_id: club.id, species_id: animalspecies[rand(0..animalspecies.size - 1)].id)
+    ClubSpecy.create(club_id: club.id, species_id: species[rand(0..species.size - 1)].id)
   end
 end
-
 puts "Club Species created"
 
+
 puts "Creating Articles..."
+
 clubs.each do |club|
   rand(2..4).times do
     Article.create(title: "Nine reasons to fight for a global wildlife trade ban", description: "We’re calling for an end to the cruel global wildlife trade – to prevent animal suffering, protect our planet and help prevent further zoonotic disease outbreaks like COVID-19. But we can’t do it without you", club_id: club.id)
