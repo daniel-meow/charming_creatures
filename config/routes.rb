576e8d31552b81d0ccd1e087b85674538345227d
Andreas Broska <a.broska@gmx.net>
2021-03-09 11:10:38 +0100

    edit club

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
+
 Rails.application.routes.draw do
   get 'pdf/show'
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :clubs, only: [ :show, :index, :new, :create, :destroy] do 
+  resources :clubs do 
     resources :donations, only: [ :new, :create]
     resources :articles, only: [ :new, :create ]
   end
