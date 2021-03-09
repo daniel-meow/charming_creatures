47d31b0c10d9ea8feb974a63b42421193993a927
Leonard Diemer <leonard.ow.diemer@gmail.com>
2021-03-09 18:14:42 +0100

    changed the user controller so the bar works

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,6 @@
 class UsersController < ApplicationController
   def show
     @user = current_user
-    # @user.goal = 90.00
 
     if @user.goal
       @donations = @user.donations.map {|donation| donation.amount_cents}.sum / 100
@@ -10,17 +9,17 @@ class UsersController < ApplicationController
   end
 
   def update_goal
-    current_user.goal = user_params[:goal]
+    current_user.goal = params[:goal]
     current_user.save!
     redirect_to profile_path
   end
 
   private
 
-  def user_params
-    params.permit(:goal, :authenticity_token, :commit)
+  # def user_params
+  #   # params.permit(:goal, :authenticity_token, :commit)
 
-    # raise
-    @clubs = Club.where(user_id: @user.id)
-  end
+  #   # raise
+  #   @clubs = Club.where(user_id: @user.id)
+  # end
 end
