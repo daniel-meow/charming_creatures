77ce22c3b6834f04e4983998f190b2c504363115
Andreas Broska <a.broska@gmx.net>
2021-03-09 14:31:19 +0100

    mapfix

--- app/javascript/channels/index.js
+++ app/javascript/channels/index.js
@@ -14,15 +14,17 @@ var btn = document.getElementById("myBtn1");
 var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on the button, open the modal
+if (btn) {
 btn.onclick = function() {
   modal.style.display = "block";
 }
-
+}
 // When the user clicks on <span> (x), close the modal
+if (span) {
 span.onclick = function() {
   modal.style.display = "none";
 }
-
+}
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target == modal) {
