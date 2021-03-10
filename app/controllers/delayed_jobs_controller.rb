7386429baf0de14cdb7286bd49b6148e1a75d804
Daniel Thiel <dthiel.berlin@gmail.com>
2021-03-10 10:17:20 +0100

    fix

--- app/controllers/delayed_jobs_controller.rb
+++
@@ -1,6 +0,0 @@
-class DelayedJobsController < ApplicationController
-  update_progress(step: 10)  # increase the progress_current for step
-  update_stage('name of stage')  # change the progress_stage
-  update_stage_progress('name of stage', step: 11)  # change progress_stage and increase progress_current for step
-  update_progress_max(progress_max) # change progress_max
-end
